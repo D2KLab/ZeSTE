76b7fbd2860b89a05230d595c93ebe0dbf78bc3f
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2021-03-24 14:41:11 +0100

    server: add swagger ui

--- UI/server/server.py
+++ UI/server/server.py
@@ -1,15 +1,14 @@
 from fast_autocomplete import AutoComplete
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Blueprint
 from flask_cors import CORS, cross_origin
 from zeste import predict
 import os
 import json
 import trafilatura
 
-
-def do_things_to_external_uri(uri):
-    # To be done later
-    pass
+import werkzeug
+werkzeug.cached_property = werkzeug.utils.cached_property
+from flask_restplus import Resource, Api
 
 
 print('Loading autocomplete vocabulary...')
@@ -24,75 +23,84 @@ autocomplete = AutoComplete(words=words)
 
 print('Starting web server...')
 app = Flask(__name__)
+api = Api(app)
 cors = CORS(app)
+ns = api.namespace('', description='Topic Prediction API')
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-@app.route('/autocomplete', methods=['GET'])
-@cross_origin()
-def autocomplete_route():
-    q = request.args.get('q')
-    suggestions = autocomplete.search(word=q, max_cost=3, size=7)
-    return jsonify(suggestions)
+@ns.route('/status')
+class status_route(Resource):
+    def get(self):
+        return 'OK'
 
 
-@app.route('/predict', methods=['POST'])
-@cross_origin()
-def predict_route():
-    content = request.json
-    print(content)
-
-    if 'uri' in content:
-        downloaded = trafilatura.fetch_url(content['uri'])
-        if downloaded is None:
-            return jsonify({ "error": "Could not fetch URL" })
-        text = trafilatura.extract(downloaded)
-    elif 'text' in content:
-        text = content['text']
-
-    # Process text with labels...
-    labels = content['labels'].split(';')
-
-    # Process text with labels
-    response = predict(text, labels)
-
-    ### response looks like this ###
-    ### (both the labels and the paths are sorted by score ###
-    # [{'label': 'space',
-    #   'score': 1.2366091944277287,
-    #   'terms': [{'paths': [['space', 'label']], 'score': 1.0},
-    #    {'paths': [['star', 'locatedat', 'space']], 'score': 0.18517242},
-    #    {'paths': [['love', 'isa', 'television_show'],
-    #      ['television_show', 'isa', 'space']],
-    #     'score': 0.05143677}]},
-    #  {'label': 'technology',
-    #   'score': 0.1451974897645414,
-    #   'terms': [{'paths': [['space', 'relatedto', 'science_fiction'],
-    #      ['science_fiction', 'relatedto', 'technology']],
-    #     'score': 0.14295651},
-    #    {'paths': [['love', 'relatedto', 'technophilia'],
-    #      ['technophilia', 'relatedto', 'technology']],
-    #     'score': 0.0022409796}]},
-    #  {'label': 'medicine',
-    #   'score': 0.05455923452973366,
-    #   'terms': [{'paths': [['space', 'relatedto', 'science'],
-    #      ['science', 'relatedto', 'medicine']],
-    #     'score': 0.054559235}]}]
-
-    # Return the output as a JSON string
-    return jsonify({
-        "text": text,
-        "labels": labels,
-        "results": response
-    })
+@ns.route('/autocomplete', methods=['GET'])
+@api.doc(params={'q': 'Search keywords'})
+class autocomplete_route(Resource):
+    @ns.doc('autocomplete_route')
+    def get(self):
+        q = request.args.get('q')
+        suggestions = autocomplete.search(word=q, max_cost=3, size=7)
+        return jsonify(suggestions)
 
 
-@app.errorhandler(Exception)
-def exception_handler(e):
-    return jsonify({ "error": str(repr(e)) })
+@ns.route('/predict', methods=['POST'])
+@api.doc(params={'uri': 'Page URI to extract text from and predict topics'})
+@api.doc(params={'text': 'Text used to extract and predict topics'})
+@api.doc(params={'labels': 'Labels separated by ";"'})
+class predict_route(Resource):
+    @ns.doc('predict_route')
+    def post(self):
+        content = request.json
+        print(content)
+
+        if 'uri' in content:
+            downloaded = trafilatura.fetch_url(content['uri'])
+            if downloaded is None:
+                return jsonify({ "error": "Could not fetch URL" })
+            text = trafilatura.extract(downloaded)
+        elif 'text' in content:
+            text = content['text']
+
+        # Process text with labels...
+        labels = content['labels'].split(';')
+
+        # Process text with labels
+        response = predict(text, labels)
+
+        ### response looks like this ###
+        ### (both the labels and the paths are sorted by score ###
+        # [{'label': 'space',
+        #   'score': 1.2366091944277287,
+        #   'terms': [{'paths': [['space', 'label']], 'score': 1.0},
+        #    {'paths': [['star', 'locatedat', 'space']], 'score': 0.18517242},
+        #    {'paths': [['love', 'isa', 'television_show'],
+        #      ['television_show', 'isa', 'space']],
+        #     'score': 0.05143677}]},
+        #  {'label': 'technology',
+        #   'score': 0.1451974897645414,
+        #   'terms': [{'paths': [['space', 'relatedto', 'science_fiction'],
+        #      ['science_fiction', 'relatedto', 'technology']],
+        #     'score': 0.14295651},
+        #    {'paths': [['love', 'relatedto', 'technophilia'],
+        #      ['technophilia', 'relatedto', 'technology']],
+        #     'score': 0.0022409796}]},
+        #  {'label': 'medicine',
+        #   'score': 0.05455923452973366,
+        #   'terms': [{'paths': [['space', 'relatedto', 'science'],
+        #      ['science', 'relatedto', 'medicine']],
+        #     'score': 0.054559235}]}]
+
+        # Return the output as a JSON string
+        return jsonify({
+            "text": text,
+            "labels": labels,
+            "results": response
+        })
+
+
+@api.errorhandler
+def default_error_handler(error):
+    return {'error': str(error)}, getattr(error, 'code', 500)
 
 
 if __name__ == '__main__':
